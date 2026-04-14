0e3aaaa0d332c896021d6560c4b0f8978b735c35
Orkoliator <lykshik@gmail.com>
2026-04-14 16:05:33 +0200

    environment variables support added

--- config/configuration_module.py
+++ config/configuration_module.py
@@ -5,18 +5,31 @@ class Config:
     
     def __init__(self):
 
-        with open("config.yaml") as config_file:
-            config = yaml.safe_load(config_file)
+        if os.path.exists("config.yaml"):
+            with open("config.yaml") as config_file:
+                config = yaml.safe_load(config_file)
 
-            self.notion_oauth = config["nt_internal_oauth_secret"]
+                self.notion_oauth = config["nt_internal_oauth_secret"]
 
-            self.google_ai_api_key = config["google_ai_api_key"]
+                self.google_ai_api_key = config["google_ai_api_key"]
 
-            self.telegram_api_id = config["telegram_api_id"]
-            self.telegram_api_hash = config["telegram_api_hash"]
-            self.telegram_bot_token = config["telegram_bot_token"]
+                self.telegram_api_id = config["telegram_api_id"]
+                self.telegram_api_hash = config["telegram_api_hash"]
+                self.telegram_bot_token = config["telegram_bot_token"]
 
-            self.db_file = config["db_file"]
+                self.db_file = config["db_file"]
+
+        else:
+            self.notion_oauth = os.getenv("NT_INTERNAL_OAUTH_SECRET")
+            
+            self.google_ai_api_key = os.getenv("GOOGLE_AI_API_KEY")
+
+            self.telegram_api_id = os.getenv("TELEGRAM_API_ID")
+            self.telegram_api_hash = os.getenv("TELEGRAM_API_HASH")
+            self.telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+
+            self.db_file = os.getenv("LOCAL_DB_FILE", "notion-tg-bot.db")
+
         with open("config/prompt.txt") as config_file:
             self.workout_base_prompt = config_file.read()
         
