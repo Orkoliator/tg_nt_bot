7fdb5d811ae6bb6119835607d563a2bac0e735b5
Orkoliator <lykshik@gmail.com>
2026-04-17 22:38:29 +0200

    debug

--- config/configuration_module.py
+++ config/configuration_module.py
@@ -9,9 +9,11 @@ class Config:
             app_path_divider = '/'
         else:
             app_path_divider = '\\'
-        self.root_dir = os.path.join( os.path.dirname( __file__ ), '..' ) + app_path_divider
+        #self.root_dir_path = os.path.join( os.path.dirname( __file__ ), '..' ) + app_path_divider
+        self.root_dir_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+        self.db_dir_path = self.root_dir_path + app_path_divider + "db"
 
-        if os.path.exists(f"{self.root_dir}config.yaml"):
+        if os.path.exists(f"{self.root_dir_path}{app_path_divider}config.yaml"):
             with open("config.yaml") as config_file:
                 config = yaml.safe_load(config_file)
 
@@ -23,7 +25,7 @@ class Config:
                 self.telegram_api_hash = config["telegram_api_hash"]
                 self.telegram_bot_token = config["telegram_bot_token"]
 
-                self.db_file = self.root_dir + 'db' + app_path_divider + config["db_file"]
+                self.db_file = self.root_dir_path + app_path_divider + config["db_file"]
 
         else:
             self.notion_oauth = os.getenv("NT_INTERNAL_OAUTH_SECRET")
@@ -34,8 +36,8 @@ class Config:
             self.telegram_api_hash = os.getenv("TELEGRAM_API_HASH")
             self.telegram_bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 
-            print(f"self.root_dir: {self.root_dir}\napp_path_divider: {app_path_divider}\nDB_FILE: {os.getenv("DB_FILE")}")
-            self.db_file = self.root_dir + 'db' + app_path_divider + os.getenv("DB_FILE")
+            print(f"self.root_dir_path: {self.root_dir_path}\napp_path_divider: {app_path_divider}\nDB_FILE: {os.getenv("DB_FILE")}")
+            self.db_file = self.root_dir_path + app_path_divider + os.getenv("DB_FILE")
 
 
         with open("config/prompt.txt") as config_file:
